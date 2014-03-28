45a6196ec55871d37b782fe7a3c079112f5efacc
thetemplateblog <david@thetemplateblog.com>
2014-03-28 10:14:10 -0400

    Update vertical-tabs.php

--- html/mod_articles_latest/vertical-tabs.php
+++ html/mod_articles_latest/vertical-tabs.php
@@ -6,7 +6,16 @@
  * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-
+ /* =====================================================================
+  Template: JoomberUI for Joomla 3.x                       
+  Author:   David Childs - Joomber.com! - @TheTemplateBlog                    
+  Version:  1.0                                    
+  Created:  March 2014                                                    
+  Copyright:  David Childs - (C) 2014 - All rights reserved   
+  Licenses: GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
+        DBAD License http://philsturgeon.co.uk/code/dbad-license
+  Source:   J3.x mod_articles_latest/                              
+/* ===================================================================== */
 defined('_JEXEC') or die;
 ?>
 
