4df4db385e6eecb0b6d6138af5bd6d436f7d2757
thetemplateblog <david@thetemplateblog.com>
2014-03-28 09:04:28 -0400

    Create horizontal-tabs.php

---
+++ html/mod_articles_latest/horizontal-tabs.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_articles_latest
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+?>
+
+<div class="<?php echo $moduleclass_sfx; ?>">
+  <dl class="tabs" data-tab>
+    <?php $first = true; ?>
+    <?php foreach ($list as $item) :  ?>
+    <?php $title = preg_replace('/\s+/', '', $item->title); ?>
+      <?php if ( $first ) { ?>
+        <dd class="active"><a href="#<?php echo $title; ?>"><?php echo $item->title; ?></a></dd>
+      <?php $first = false; } else { ?>
+        <dd><a href="#<?php echo $title; ?>"><?php echo $item->title; ?></a></dd>
+      <?php } ?>
+    <?php endforeach; ?>
+  </dl>
+  <div class="tabs-content">
+    <?php $first = true; ?>
+    <?php foreach ($list as $item) :  ?>
+    <?php $title = preg_replace('/\s+/', '', $item->title); ?>
+      <?php if ( $first ) { ?>
+        <div class="content active" id="<?php echo $title; ?>">
+          <?php $first = false; } else { ?>
+        <div class="content" id="<?php echo $title; ?>">
+          <?php } ?>
+        <h4><?php echo $item->title; ?></h4>
+        <?php echo $item->introtext; ?>
+        <a href="<?php echo $item->link; ?>" class="button small radius info">Read more</a>
+      </div>
+    <?php endforeach; ?>
+  </div>
+</div>
